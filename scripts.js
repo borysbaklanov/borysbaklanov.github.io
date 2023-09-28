da7480da1277662e134a937a02aa8f94192fdb6d
Borys Baklanov <borys.baklanov@gmail.com>
2023-09-28 13:47:48 +0200

    starting mobilku

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
 const words = ["Danke.", "Thank you.", "Дякую."];
-
 function getRandomWord() {
     const randomIndex = Math.floor(Math.random() * words.length);
     return words[randomIndex];
 }
-
 function changeText() {
     const randomTextElement = document.getElementById("ty");
     randomTextElement.textContent = getRandomWord();
 }
-
 window.addEventListener("load", changeText);
+
+
+
+const textElement = document.getElementById("logo");
+textElement.addEventListener("mouseover", () => {
+    textElement.textContent = "Борис Бакланов";
+});
+
+textElement.addEventListener("mouseout", () => {
+    textElement.textContent = "Borys Baklanov";
+});
