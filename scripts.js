489328eab9042d22f089c59f9e3ffba19cd65f8e
Borys Baklanov <borys.baklanov@gmail.com>
2023-09-29 14:45:06 +0200

    almost done
    the only things left to finish is:
    — logo fix for desktop+mobile
    — table
    — smth else

--- scripts.js
+++ scripts.js
@@ -10,12 +10,14 @@ function changeText() {
 window.addEventListener("load", changeText);
 
 
+if (window.matchMedia('(min-width: 768px)').matches) {
+    const textElement = document.getElementById("logo");
+    logo.style.pointerEvents = "auto";
+    textElement.addEventListener("mouseover", () => {
+        textElement.textContent = "Борис Бакланов";
+    });
 
-const textElement = document.getElementById("logo");
-textElement.addEventListener("mouseover", () => {
-    textElement.textContent = "Борис Бакланов";
-});
-
-textElement.addEventListener("mouseout", () => {
-    textElement.textContent = "Borys Baklanov";
-});
+    textElement.addEventListener("mouseout", () => {
+        textElement.textContent = "Borys Baklanov";
+    });
+}
