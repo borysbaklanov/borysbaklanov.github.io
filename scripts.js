6bf2e2894a37807d154b2271a919ce34130f9232
Borys Baklanov <borys.baklanov@gmail.com>
2023-10-07 16:29:04 +0200

    fixed logo and other things

--- scripts.js
+++ scripts.js
@@ -11,13 +11,18 @@ window.addEventListener("load", changeText);
 
 
 if (window.matchMedia('(min-width: 768px)').matches) {
-    const textElement = document.getElementById("logo");
-    logo.style.pointerEvents = "auto";
+    const textElement = document.getElementById("logo-container");
     textElement.addEventListener("mouseover", () => {
         textElement.textContent = "Борис Бакланов";
     });
-
     textElement.addEventListener("mouseout", () => {
         textElement.textContent = "Borys Baklanov";
     });
+
+    const linkElement = document.getElementById("logo-link");
+    const linkText = document.createTextNode(linkElement.textContent);
+    const parentElement = linkElement.parentNode;
+    parentElement.replaceChild(linkText, linkElement);
+
+
 }
